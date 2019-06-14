0754606641885410693f3ebd10ac9097df626173
Nishant Shaligram <nishant.shaligram@gmail.com>
2019-06-15 00:54:45 +0530

    Front Page Updated
    Front Page Updated

--- front-page.php
+++ front-page.php
@@ -1,7 +1,23 @@
 <?php
 
 get_header();
+?>
 
-get_template_part( 'template-parts/page/content' );
+<div id="page" class="site-content">
+    <div class="container">
+        <div class="row">
+            <div id="primary" class="content-area col-md-12 col-sm-12">
+            <?php
+            if( have_posts() ):
+                while( have_posts() ): the_post();
+                    get_template_part( 'template-parts/page/content' );
+                endwhile;
+            endif;
+            ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
 
 get_footer();
