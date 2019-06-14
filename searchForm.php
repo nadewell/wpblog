5c929b87067f5e94791433538b43bfa679d19fb1
Nishant Shaligram <nishant.shaligram@gmail.com>
2019-06-15 01:08:31 +0530

    SearchForm template added
    SearchForm template added

---
+++ searchForm.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" id="searchform" class="searchform" action="https://www.nadewell.tk/">
+    <div class="form-group">
+        <label class="screen-reader-text" for="s">Search for:</label>
+        <input class="form-control" type="text" value="" name="s" id="s">
+        <button class="btn btn-primary" type="submit" id="searchsubmit"><i class="fa fa-search"></i></button>
+    </div>
+</form>
