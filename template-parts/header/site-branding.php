e1c03710c4ea7a64685eede61adb6280ad2ae515
Nishant Shaligram <nishant.shaligram@gmail.com>
2019-06-15 18:36:04 +0530

    Logo CSS added
    Logo CSS added

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -1,1 +1,11 @@
-<a class="navbar-brand" href="#">Navbar</a>
+<?php 
+
+
+$custom_logo = get_theme_mod( 'custom_logo' );
+$site_title = get_bloginfo('name');
+
+if($custom_logo != ''){ 
+    echo '<a class="navbar-brand" href="'.site_url().'" title="'.$site_title.'"><img src="'.wp_get_attachment_image_src( $custom_logo, 'full' )[0].'" alt="'.$site_title.'"></a>';
+}else{
+    echo '<a class="navbar-brand" href="'.site_url().'">'.$site_title.'</a>';
+}
